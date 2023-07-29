4702163d1453e9b05eca1cbfed7d9fa6b51a6ba2
flashrifle <flashrifle@gmail.com>
2023-07-29 17:53:25 +0900

    js dom 과제 - 1

--- 04-javascript-dom/js/dom_ex.js
+++ 04-javascript-dom/js/dom_ex.js
@@ -1,1 +1,25 @@
-console.log(document.getElementById('value1'));
+const inputs = document.querySelectorAll('input');
+const val1 = document.getElementById('value1');
+const val2 = document.getElementById('value2');
+const calc = document.getElementById('calc');
+const result = document.getElementById('result');
+
+const sub = document.getElementById('sub');
+sub.addEventListener('click', function () {
+    if (calc.value === '+') {
+        result.value = Number(val1.value) + Number(val2.value);
+    } else if (calc.value === '-') {
+        result.value = Number(val1.value) - Number(val2.value);
+    } else if (calc.value === '*') {
+        result.value = Number(val1.value) * Number(val2.value);
+    } else if (calc.value === '/') {
+        result.value = Number(val1.value) / Number(val2.value);
+    }
+});
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', function () {
+    for (let input of inputs) {
+        input.value = '';
+    }
+});
