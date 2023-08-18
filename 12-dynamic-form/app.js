01ff063d279cef733343bc9d4ae390ecb1c78559
flashrifle <flashrifle@gmail.com>
2023-08-18 17:16:25 +0900

    study: 동적 폼 전송 - axios, fetch

--- 12-dynamic-form/app.js
+++ 12-dynamic-form/app.js
@@ -28,3 +28,23 @@ app.post('/ajax', (req, res) => {
     console.log(req.body);
     res.send(req.body);
 });
+
+app.get('/axios', (req, res) => {
+    console.log(req.query);
+    res.send(res.query);
+});
+
+app.post('/axios', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.get('/fetch', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+});
+
+app.post('/fetch', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
