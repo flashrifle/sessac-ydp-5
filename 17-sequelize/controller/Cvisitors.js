0c605c0cb3dcc2272dd67e4f2e054d90bf249d46
flashrifle <flashrifle@gmail.com>
2023-08-30 18:24:56 +0900

    stduy: squelize 사용해 controller, model 리팩토링

--- 17-sequelize/controller/Cvisitors.js
+++ 17-sequelize/controller/Cvisitors.js
@@ -1,57 +1,93 @@
-const Visitor = require('../model/Visitor');
+// const Visitor = require('../model/Visitor');
+const { Visitor } = require('../models'); // ../models/index.js
 
 exports.main = (req, res) => {
     res.render('index');
 };
 
-exports.getVisitors = (req, res) => {
-    // console.log(Visitor.getVisitors());
-    Visitor.getVisitors((result) => {
-        console.log('Cvisitor.js : ', result);
-        res.render('visitor', {
-            data: result,
-        });
-    });
+// read all
+exports.getVisitors = async (req, res) => {
+    // [before]
+    // Visitor.getVisitors((result) => {
+    //   console.log('controller >>', result);
+    //   res.render('visitor', { data: result });
+    // });
+
+    // [after]
+    const result = await Visitor.findAll();
+    console.log(result);
+    res.render('visitor', { data: result });
 };
 
-exports.postVisitor = (req, res) => {
-    console.log(req.body);
+// create
+exports.postVisitor = async (req, res) => {
+    // [before]
+    // console.log(req.body); // { name: xx, comment: yy }
+    // const { name, comment } = req.body;
 
+    // Visitor.postVisitor(req.body, (insertId) => {
+    //   console.log('controller >> ', insertId);
+    //   res.send({ id: insertId, name: name, comment: comment });
+    // });
+
+    // [after]
     const { name, comment } = req.body;
-
-    Visitor.postVisitor(req.body, (insertId) => {
-        console.log('controller >> ', insertId);
-        res.send({
-            id: insertId,
-            name: name,
-            comment: comment,
-        });
+    const result = await Visitor.create({
+        name,
+        comment,
     });
+    // console.log(result); // create메서드가 실행된 결과 (== insert 한 데이터 값)
+    res.send(result);
 };
 
-exports.deleteVisitor = (req, res) => {
-    console.log(req.body);
+exports.deleteVisitor = async (req, res) => {
+    // [before]
+    // console.log(req.body);
+    // const { id } = req.body;
+
+    // Visitor.deleteVisitor(id, (result) => {
+    //     console.log('controller >> ', result);
+    //     res.send(result);
+    // });
     const { id } = req.body;
-
-    Visitor.deleteVisitor(id, (result) => {
-        console.log('controller >> ', result);
-        res.send(result);
+    const result = await Visitor.destroy({
+        where: { id: id },
     });
+    res.send(true);
 };
 
-exports.getVisitor = (req, res) => {
-    console.log(req.params);
+exports.getVisitor = async (req, res) => {
+    // [before]
+    // console.log(req.params);
+    // const { id } = req.params;
+
+    // Visitor.getVisitor(id, (result) => {
+    //     console.log('controller >> ', result);
+    //     res.send(result);
+    // });
+
+    // [after]
     const { id } = req.params;
-
-    Visitor.getVisitor(id, (result) => {
-        console.log('controller >> ', result);
-        res.send(result);
+    const result = await Visitor.findOne({
+        where: { id: id },
     });
+    res.send(result);
 };
 
-exports.updateVisitor = (req, res) => {
-    console.log(req.body);
-    Visitor.updateVisitor(req.body, () => {
-        res.send({ isUpdated: true });
-    });
+exports.updateVisitor = async (req, res) => {
+    // [before]
+    // console.log(req.body);
+    // Visitor.updateVisitor(req.body, () => {
+    //     res.send({ isUpdated: true });
+    // });
+
+    // [after]
+    await Visitor.update(
+        {
+            name: req.body.name,
+            comment: req.body.comment,
+        },
+        { where: { id: req.body.id } }
+    );
+    res.send({ isUpdated: true });
 };
