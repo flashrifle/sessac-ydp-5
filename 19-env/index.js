cad6172e0927aa4d7707940126196e110e675324
flashrifle <flashrifle@gmail.com>
2023-09-04 15:16:07 +0900

    study: dotenv를 이용한 환경변수 관리

---
+++ 19-env/index.js
@@ -0,0 +1,18 @@
+// const ps = process.env;
+
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    console.log(process.env.NAME);
+    console.log(process.env.NODE);
+    res.send('hello world');
+});
+
+app.listen(PORT, () => {
+    console.log(`${PORT} conn!`);
+});
